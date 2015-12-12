44de3dcd5646b4726ab1afbf7fad795780bd0cfb
Antoine Jacoutot <ajacoutot@gnome.org>
2015-12-12 21:38:22 +0100

    Fix logic.

--- create-ami.sh
+++ create-ami.sh
@@ -64,51 +64,51 @@ touch ${_LOG}
 trap "cat ${_LOG}" ERR
 
 echo "===> create image container"
-doas dd if=/dev/zero of=${_IMG} bs=1m count=1024 >${_LOG} 2>&1
+doas dd if=/dev/zero of=${_IMG} bs=1m count=1024 2>&1 >${_LOG}
 
 echo "===> create image filesystem"
-doas vnconfig ${_VNDEV} ${_IMG} >${_LOG} 2>&1
-doas fdisk -iy ${_VNDEV} >${_LOG} 2>&1
-printf "a\n\n\n\n\nq\n\n" | doas disklabel -E ${_VNDEV} >${_LOG} 2>&1
-doas newfs /dev/r${_VNDEV}a >${_LOG} 2>&1
+doas vnconfig ${_VNDEV} ${_IMG} 2>&1 >${_LOG}
+doas fdisk -iy ${_VNDEV} 2>&1 >${_LOG}
+printf "a\n\n\n\n\nq\n\n" | doas disklabel -E ${_VNDEV} 2>&1 >${_LOG}
+doas newfs /dev/r${_VNDEV}a 2>&1 >${_LOG}
 
 echo "===> mount image"
-doas mount /dev/${_VNDEV}a ${_MNT} >${_LOG} 2>&1
+doas mount /dev/${_VNDEV}a ${_MNT} 2>&1 >${_LOG}
 
 echo "===> fetch sets (can take some time)"
 ( cd ${_WRKDIR} && \
-	ftp -V ${MIRROR}/{bsd{,.mp,.rd},{base,comp,game,man,xbase,xshare,xfont,xserv}${_REL}.tgz} >${_LOG} 2>&1 )
+	ftp -V ${MIRROR}/{bsd{,.mp,.rd},{base,comp,game,man,xbase,xshare,xfont,xserv}${_REL}.tgz} 2>&1 >${_LOG} )
 
 echo "===> extract sets"
-for i in ${_WRKDIR}/*${_REL}.tgz ${_MNT}/var/sysmerge/{,x}etc.tgz; do doas tar xzphf $i -C ${_MNT} >${_LOG} 2>&1; done
+for i in ${_WRKDIR}/*${_REL}.tgz ${_MNT}/var/sysmerge/{,x}etc.tgz; do doas tar xzphf $i -C ${_MNT} 2>&1 >${_LOG}; done
 
 echo "===> install MP kernel"
-doas mv ${_WRKDIR}/bsd* ${_MNT} >${_LOG} 2>&1
-doas mv ${_MNT}/bsd ${_MNT}/bsd.sp >${_LOG} 2>&1
-doas mv ${_MNT}/bsd.mp ${_MNT}/bsd >${_LOG} 2>&1
-doas chown 0:0 ${_MNT}/bsd* >${_LOG} 2>&1
+doas mv ${_WRKDIR}/bsd* ${_MNT} 2>&1 >${_LOG}
+doas mv ${_MNT}/bsd ${_MNT}/bsd.sp 2>&1 >${_LOG}
+doas mv ${_MNT}/bsd.mp ${_MNT}/bsd 2>&1 >${_LOG}
+doas chown 0:0 ${_MNT}/bsd* 2>&1 >${_LOG}
 
 echo "===> remove downloaded files"
-rm ${_WRKDIR}/*${_REL}.tgz >${_LOG} 2>&1
+rm ${_WRKDIR}/*${_REL}.tgz 2>&1 >${_LOG}
 
 echo "===> create devices"
-( cd ${_MNT}/dev && doas sh ./MAKEDEV all >${_LOG} 2>&1 )
+( cd ${_MNT}/dev && doas sh ./MAKEDEV all 2>&1 >${_LOG} )
 
 echo "===> install master boot record"
-doas installboot -r ${_MNT} ${_VNDEV} >${_LOG} 2>&1
+doas installboot -r ${_MNT} ${_VNDEV} 2>&1 >${_LOG}
 
 echo "===> configure the image"
-echo "/dev/wd0a / ffs rw 1 1" | doas tee ${_MNT}/etc/fstab >${_LOG} 2>&1
-doas sed -i "s,^tty00.*,tty00	\"/usr/libexec/getty std.9600\"	vt220   on  secure," ${_MNT}/etc/ttys >${_LOG} 2>&1
-echo "stty com0 9600" | doas tee ${_MNT}/etc/boot.conf >${_LOG} 2>&1
-echo "tty com0" | doas tee -a ${_MNT}/etc/boot.conf >${_LOG} 2>&1
-doas chroot ${_MNT} ln -sf /usr/share/zoneinfo/UTC /etc/localtime >${_LOG} 2>&1
-doas chroot ${_MNT} ldconfig /usr/local/lib /usr/X11R6/lib >${_LOG} 2>&1
-doas chroot ${_MNT} rcctl disable sndiod >${_LOG} 2>&1
-doas umount ${_MNT} >${_LOG} 2>&1
+echo "/dev/wd0a / ffs rw 1 1" | doas tee ${_MNT}/etc/fstab 2>&1 >${_LOG}
+doas sed -i "s,^tty00.*,tty00	\"/usr/libexec/getty std.9600\"	vt220   on  secure," ${_MNT}/etc/ttys 2>&1 >${_LOG}
+echo "stty com0 9600" | doas tee ${_MNT}/etc/boot.conf 2>&1 >${_LOG}
+echo "tty com0" | doas tee -a ${_MNT}/etc/boot.conf 2>&1 >${_LOG}
+doas chroot ${_MNT} ln -sf /usr/share/zoneinfo/UTC /etc/localtime 2>&1 >${_LOG}
+doas chroot ${_MNT} ldconfig /usr/local/lib /usr/X11R6/lib 2>&1 >${_LOG}
+doas chroot ${_MNT} rcctl disable sndiod 2>&1 >${_LOG}
+doas umount ${_MNT} 2>&1 >${_LOG}
 
 echo "===> unmount the image"
-doas vnconfig -u ${_VNDEV} >${_LOG} 2>&1
+doas vnconfig -u ${_VNDEV} 2>&1 >${_LOG}
 
 echo "===> image available at:"
 echo "     ${_IMG}"
