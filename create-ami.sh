cfe9920faec179adb9659800431ac8e7109628f3
Antoine Jacoutot <ajacoutot@gnome.org>
2016-01-06 10:29:16 +0100

    Allow using an existing image.

--- create-ami.sh
+++ create-ami.sh
@@ -30,7 +30,6 @@ AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 AWS_REGION=eu-west-1
 AWS_AZ=eu-west-1a
-AWS_CREATE_AMI=YES
 
 IMGSIZE=1 # GB
 MIRROR=http://ftp.fr.openbsd.org/pub/OpenBSD/snapshots/${_ARCH}
@@ -49,98 +48,97 @@ if [[ $(doas ${RANDOM} 2>/dev/null) == 1 ]]; then
 	exit 1
 fi
 
-if [[ ${AWS_CREATE_AMI} ==  "YES" ]]; then
-	if [[ -z ${AWS_ACCESS_KEY_ID} || -z ${AWS_SECRET_ACCESS_KEY} ]]; then
-		echo "${0##*/}: AWS credentials aren't set"
-		exit 1
-	fi
-	if ! type ec2-import-volume >/dev/null; then
-		echo "${0##*/}: needs the EC2 CLI tools"
-	fi
-	unset _VOL
-fi
-
 set -e
 umask 022
 
-_WRKDIR=$(mktemp -d -p /tmp aws-ami.XXXXXXXXXX)
-_LOG=${_WRKDIR}/log
-_IMG=${_WRKDIR}/img-openbsd-$(date "+%s")
-_MNT=${_WRKDIR}/mnt
-_REL=$(uname -r | tr -d '.')
-_VNDEV=$(doas vnconfig -l | grep 'not in use' | head -1 | cut -d ':' -f1)
-
-if [[ -z ${_VNDEV} ]]; then
-	echo "${0##*/}: no vnd(4) device available"
+usage() {
+	echo "usage: ${0##*/} [-in]" >&2
+	echo "       -i /path/to/image" >&2
+	echo "       -n don't create an AMI" >&2
 	exit 1
-fi
+}
 
-mkdir -p ${_MNT}
-touch ${_LOG}
+create_img() {
+	_WRKDIR=$(mktemp -d -p /tmp aws-ami.XXXXXXXXXX)
+	_IMG=${_WRKDIR}/img-openbsd-$(date "+%s")
+	local _LOG=${_WRKDIR}/log
+	local _MNT=${_WRKDIR}/mnt
+	local _REL=$(uname -r | tr -d '.')
+	local _VNDEV=$(doas vnconfig -l | grep 'not in use' | head -1 | cut -d ':' -f1)
 
-trap "cat ${_LOG}" ERR
+	if [[ -z ${_VNDEV} ]]; then
+		echo "${0##*/}: no vnd(4) device available"
+		exit 1
+	fi
 
-echo "===> create image container"
-doas dd if=/dev/zero of=${_IMG} bs=1m count=$((${IMGSIZE}*1024)) >${_LOG} 2>&1
+	mkdir -p ${_MNT}
+	touch ${_LOG}
 
-echo "===> create image filesystem"
-doas vnconfig ${_VNDEV} ${_IMG} >${_LOG} 2>&1
-doas fdisk -iy ${_VNDEV} >${_LOG} 2>&1
-printf "a\n\n\n\n\nq\n\n" | doas disklabel -E ${_VNDEV} >${_LOG} 2>&1
-doas newfs /dev/r${_VNDEV}a >${_LOG} 2>&1
+	trap "cat ${_LOG}" ERR
 
-echo "===> mount image"
-doas mount /dev/${_VNDEV}a ${_MNT} >${_LOG} 2>&1
+	echo "===> create image container"
+	doas dd if=/dev/zero of=${_IMG} bs=1m count=$((${IMGSIZE}*1024)) >${_LOG} 2>&1
 
-echo "===> fetch sets (can take some time)"
-( cd ${_WRKDIR} && \
-	ftp -V ${MIRROR}/{bsd{,.mp,.rd},{base,comp,game,man,xbase,xshare,xfont,xserv}${_REL}.tgz} >${_LOG} 2>&1 )
+	echo "===> create image filesystem"
+	doas vnconfig ${_VNDEV} ${_IMG} >${_LOG} 2>&1
+	doas fdisk -iy ${_VNDEV} >${_LOG} 2>&1
+	printf "a\n\n\n\n\nq\n\n" | doas disklabel -E ${_VNDEV} >${_LOG} 2>&1
+	doas newfs /dev/r${_VNDEV}a >${_LOG} 2>&1
 
-echo "===> extract sets"
-for i in ${_WRKDIR}/*${_REL}.tgz ${_MNT}/var/sysmerge/{,x}etc.tgz; do doas tar xzphf $i -C ${_MNT} >${_LOG} 2>&1; done
+	echo "===> mount image"
+	doas mount /dev/${_VNDEV}a ${_MNT} >${_LOG} 2>&1
 
-echo "===> install MP kernel"
-doas mv ${_WRKDIR}/bsd* ${_MNT} >${_LOG} 2>&1
-doas mv ${_MNT}/bsd ${_MNT}/bsd.sp >${_LOG} 2>&1
-doas mv ${_MNT}/bsd.mp ${_MNT}/bsd >${_LOG} 2>&1
-doas chown 0:0 ${_MNT}/bsd* >${_LOG} 2>&1
+	echo "===> fetch sets (can take some time)"
+	( cd ${_WRKDIR} && \
+		ftp -V ${MIRROR}/{bsd{,.mp,.rd},{base,comp,game,man,xbase,xshare,xfont,xserv}${_REL}.tgz} >${_LOG} 2>&1 )
 
-echo "===> remove downloaded files"
-rm ${_WRKDIR}/*${_REL}.tgz >${_LOG} 2>&1
+	echo "===> extract sets"
+	for i in ${_WRKDIR}/*${_REL}.tgz ${_MNT}/var/sysmerge/{,x}etc.tgz; do doas tar xzphf $i -C ${_MNT} >${_LOG} 2>&1; done
 
-echo "===> create devices"
-( cd ${_MNT}/dev && doas sh ./MAKEDEV all >${_LOG} 2>&1 )
+	echo "===> install MP kernel"
+	doas mv ${_WRKDIR}/bsd* ${_MNT} >${_LOG} 2>&1
+	doas mv ${_MNT}/bsd ${_MNT}/bsd.sp >${_LOG} 2>&1
+	doas mv ${_MNT}/bsd.mp ${_MNT}/bsd >${_LOG} 2>&1
+	doas chown 0:0 ${_MNT}/bsd* >${_LOG} 2>&1
 
-echo "===> store entropy for the initial boot"
-doas dd if=/dev/random of=${_MNT}/var/db/host.random bs=65536 count=1 status=none >${_LOG} 2>&1
-doas dd if=/dev/random of=${_MNT}/etc/random.seed bs=512 count=1 status=none >${_LOG} 2>&1
-doas chmod 600 ${_MNT}/var/db/host.random ${_MNT}/etc/random.seed >${_LOG} 2>&1
+	echo "===> remove downloaded files"
+	rm ${_WRKDIR}/*${_REL}.tgz >${_LOG} 2>&1
 
-echo "===> install master boot record"
-doas installboot -r ${_MNT} ${_VNDEV} >${_LOG} 2>&1
+	echo "===> create devices"
+	( cd ${_MNT}/dev && doas sh ./MAKEDEV all >${_LOG} 2>&1 )
 
-echo "===> configure the image"
-echo "/dev/wd0a / ffs rw 1 1" | doas tee ${_MNT}/etc/fstab >${_LOG} 2>&1
-doas sed -i "s,^tty00.*,tty00	\"/usr/libexec/getty std.9600\"	vt220   on  secure," ${_MNT}/etc/ttys >${_LOG} 2>&1
-echo "stty com0 9600" | doas tee ${_MNT}/etc/boot.conf >${_LOG} 2>&1
-echo "set tty com0" | doas tee -a ${_MNT}/etc/boot.conf >${_LOG} 2>&1
-doas chroot ${_MNT} ln -sf /usr/share/zoneinfo/UTC /etc/localtime >${_LOG} 2>&1
-doas chroot ${_MNT} ldconfig /usr/local/lib /usr/X11R6/lib >${_LOG} 2>&1
-doas chroot ${_MNT} rcctl disable sndiod >${_LOG} 2>&1
+	echo "===> store entropy for the initial boot"
+	doas dd if=/dev/random of=${_MNT}/var/db/host.random bs=65536 count=1 status=none >${_LOG} 2>&1
+	doas dd if=/dev/random of=${_MNT}/etc/random.seed bs=512 count=1 status=none >${_LOG} 2>&1
+	doas chmod 600 ${_MNT}/var/db/host.random ${_MNT}/etc/random.seed >${_LOG} 2>&1
 
-echo "===> unmount the image"
-doas umount ${_MNT} >${_LOG} 2>&1
-doas vnconfig -u ${_VNDEV} >${_LOG} 2>&1
+	echo "===> install master boot record"
+	doas installboot -r ${_MNT} ${_VNDEV} >${_LOG} 2>&1
 
-echo "===> image available at:"
-echo "     ${_IMG}"
-echo
+	echo "===> configure the image"
+	echo "/dev/wd0a / ffs rw 1 1" | doas tee ${_MNT}/etc/fstab >${_LOG} 2>&1
+	doas sed -i "s,^tty00.*,tty00	\"/usr/libexec/getty std.9600\"	vt220   on  secure," ${_MNT}/etc/ttys >${_LOG} 2>&1
+	echo "stty com0 9600" | doas tee ${_MNT}/etc/boot.conf >${_LOG} 2>&1
+	echo "set tty com0" | doas tee -a ${_MNT}/etc/boot.conf >${_LOG} 2>&1
+	echo "dhcp" | doas tee ${_MNT}/etc/hostname.xnf0 >${_LOG} 2>&1
+	doas chroot ${_MNT} ln -sf /usr/share/zoneinfo/UTC /etc/localtime >${_LOG} 2>&1
+	doas chroot ${_MNT} ldconfig /usr/local/lib /usr/X11R6/lib >${_LOG} 2>&1
+	doas chroot ${_MNT} rcctl disable sndiod >${_LOG} 2>&1
 
-trap - ERR
-rmdir ${_MNT} || true
-rm -f ${_LOG}
+	echo "===> unmount the image"
+	doas umount ${_MNT} >${_LOG} 2>&1
+	doas vnconfig -u ${_VNDEV} >${_LOG} 2>&1
 
-if [[ ${AWS_CREATE_AMI} ==  "YES" ]]; then
+	echo "===> image available at:"
+	echo "     ${_IMG}"
+	echo
+
+	trap - ERR
+	rmdir ${_MNT} || true
+	rm -f ${_LOG}
+}
+
+create_ami(){
 	echo "===> upload image to S3 (can take some time)"
 	ec2-import-volume \
 		${_IMG} \
@@ -169,7 +167,7 @@ if [[ ${AWS_CREATE_AMI} ==  "YES" ]]; then
 
 	#echo
 	#echo "===> delete local and remote disk images"
-	rm -rf ${_WRKDIR}
+	#rm -rf ${_WRKDIR}
 	#ec2-delete-disk-image
 
 	echo
@@ -202,4 +200,33 @@ if [[ ${AWS_CREATE_AMI} ==  "YES" ]]; then
 		--root-device-name /dev/sda1 \
 		--virtualization-type hvm \
 		-s ${_SNAP}
+}
+
+CREATE_AMI=true
+CREATE_IMG=true
+while getopts i:n arg; do
+	case ${arg} in
+	i)	CREATE_IMG=false; _IMG="${OPTARG}";;
+	n)	CREATE_AMI=false;;
+	*)	usage;;
+	esac
+done
+
+if ${CREATE_IMG}; then
+	create_img
+elif [[ ! -f ${_IMG} ]]; then
+	echo "${0##*/}: ${_IMG} does not exist"
+	exit 1
+fi
+
+if ${CREATE_AMI}; then
+	if [[ -z ${AWS_ACCESS_KEY_ID} || -z ${AWS_SECRET_ACCESS_KEY} ]]; then
+		echo "${0##*/}: AWS credentials aren't set"
+		exit 1
+	fi
+	if ! type ec2-import-volume >/dev/null; then
+		echo "${0##*/}: needs the EC2 CLI tools"
+		exit 1
+	fi
+	create_ami
 fi
