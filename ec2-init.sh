794fa27fb02c24374022e606c8ae5e570d2df766
Antoine Jacoutot <ajacoutot@gnome.org>
2016-01-12 11:55:52 +0100

    Refactor everything so that it can run from hostname.if.

--- ec2-init.sh
+++ ec2-init.sh
@@ -22,69 +22,91 @@
 
 ec2_fingerprints()
 {
-	( while ! pgrep -q -xf "/usr/sbin/sshd"; do sleep 1; done
-	logger -s -t ec2 <<EOF
+	cat <<'EOF-RC' >>/etc/rc.firsttime
+logger -s -t ec2 <<EOF
 #############################################################
 -----BEGIN SSH HOST KEY FINGERPRINTS-----
 $(for _f in /etc/ssh/ssh_host_*_key.pub; do ssh-keygen -lf ${_f}; done)
 -----END SSH HOST KEY FINGERPRINTS-----
 #############################################################
 EOF
-	) &
+EOF-RC
 }
 
 ec2_hostname()
 {
-	local _hostname="$(mock meta-data/local-hostname)"
-	hostname ${_hostname}
-	echo ${_hostname} >/etc/myname
+	local _hostname="$(mock meta-data/local-hostname)" || return
+	hostname ${_hostname} || return
+	print -- "${_hostname}" >/etc/myname || return
 }
 
 ec2_pubkey()
 {
-	mkdir -pm 0700 /root/.ssh
-	echo "$(mock meta-data/public-keys/0/openssh-key)" \
-		>>/root/.ssh/authorized_keys
-	chmod 0600 /root/.ssh/authorized_keys
+	local _pubkey="$(mock meta-data/public-keys/0/openssh-key)" || return
+	install -d -m 0700 /root/.ssh || return
+	if [[ ! -f /root/.ssh/authorized_keys ]]; then
+		install -m 0600 /dev/null /root/.ssh/authorized_keys || return
+	fi
+	print -- "${_pubkey}" >>/root/.ssh/authorized_keys || return
 }
 
 # XXX only handles scripts for now
 ec2_userdata()
 {
-	local _userdata="$(mktemp -p /tmp -t aws-user-data.XXXXXXXXXX)" || return
-	echo "$(mock user-data)" >${_userdata}
-	[[ -s ${_userdata} ]] && chmod u+x ${_userdata} && \
-		/bin/sh -c ${_userdata} && rm ${_userdata}
+	local _userdata="$(mock user-data)" || return
+	local _script="$(mktemp -p /tmp -t aws-user-data.XXXXXXXXXX)" || return
+	print -- "${_userdata}" >${_script} && chmod u+x ${_script} && \
+		/bin/sh -c ${_script} && rm ${_script} || return
 }
 
 mock()
 {
 	[[ -n ${1} ]] || return
-	ftp -MVo - http://169.254.169.254/latest/${1} 2>/dev/null || return
+	local _ret
+	_ret=$(ftp -MVo - http://169.254.169.254/latest/${1} 2>/dev/null) || return
+	[[ -n ${_ret} ]] && print -- ${_ret} || return
+}
+
+mock_pf()
+{
+	[[ -z ${INRC} || ! $(rcctl get pf status ]] && return
+	case ${1} in
+		open)
+			print -- \
+			"pass out proto tcp from egress to 169.254.169.254 port www" | \
+			pfctl -f -
+			;;
+		close)
+			print -- "" | pfctl -f -
+			;;
+		*)
+			return 1
+			;;
+	esac
 }
 
 usage()
 {
-	echo "usage: ${0##*/} cloudinit|firstboot" >&2; exit 1
+	echo "usage: ${0##*/} cloudinit|firstboot"; exit 1
 }
+
+if [[ $(id -u) != 0 ]]; then
+	echo "${0##*/}: needs root privileges"
+	exit 1
+fi
 
 case ${1} in
 	cloudinit)
 		# XXX TODO
 		exit 0 ;;
 	firstboot)
-		if [[ -n "${INRC}" ]]; then
-			sed -i "/^!\/usr\/local\/libexec\/ec2-init/d" \
-				/etc/hostname.xnf0
-			print -- \
-			"pass out proto tcp from any to 169.254.169.254 port www keep state" | \
-			pfctl -f -
-		fi
+		sed -i "/^!\/usr\/local\/libexec\/ec2-init/d" /etc/hostname.xnf0
+		mock_pf open
 		ec2_pubkey
 		ec2_hostname
 		ec2_userdata
 		ec2_fingerprints
-		[[ -n "${INRC}" ]] && print -- "" | pfctl -f -
+		mock_pf close
 		;;
 	*)
 		usage ;;
