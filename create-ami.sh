b656f3b26792b46bb0ca8f62e7685fb6f20d22d7
Eric Radman <ericshane@eradman.com>
2017-02-06 13:47:19 -0500

    Force umount of aws-ami tmp build path
    On my workstation this will not unmount without -f
    
    $ doas fuser -c /home/eradman/iso/aws-ami.nBkG9TaL2R/mnt
    /home/eradman/iso/aws-ami.nBkG9TaL2R/mnt:
    
    $ doas umount /home/eradman/iso/aws-ami.nBkG9TaL2R/mnt
    umount: /home/eradman/iso/aws-ami.nBkG9TaL2R/mnt: Device busy

--- create-ami.sh
+++ create-ami.sh
@@ -209,7 +209,7 @@ EOF
 	doas umount ${_MNT}/var
 	doas umount ${_MNT}/home
 	doas umount ${_MNT}/tmp
-	doas umount ${_MNT}
+	doas umount -f ${_MNT}
 	doas vnconfig -u ${_VNDEV}
 
 	pr_action "image available at: ${_IMG}"
