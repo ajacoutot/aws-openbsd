f56023e9b7a3aa884638e0341b25abe1b7ea8c40
Antoine Jacoutot <ajacoutot@gnome.org>
2017-02-24 14:40:21 +0100

    Run as root; it makes no sense to use doas here; also we can do some unpriv() in the future.

--- create-ami.sh
+++ create-ami.sh
@@ -37,8 +37,8 @@ if [[ $(uname -m) != amd64 ]]; then
 	exit 1
 fi
 
-if [[ $(doas ${RANDOM} 2>/dev/null) == 1 ]]; then
-	echo "${0##*/}: needs doas(1) privileges"
+if (($(id -u) != 0)); then
+	echo "${0##*/}: need root privileges"
 	exit 1
 fi
 
@@ -61,7 +61,7 @@ create_img() {
 	_WRKDIR=$(mktemp -d -p ${TMPDIR:=/tmp} aws-ami.XXXXXXXXXX)
 	_IMG=${_WRKDIR}/openbsd-${RELEASE:-current}-amd64-${TIMESTAMP}
 	local _MNT=${_WRKDIR}/mnt _REL=${RELEASE:-$(uname -r)} _p
-	local _VNDEV=$(doas vnconfig -l | grep 'not in use' | head -1 |
+	local _VNDEV=$(vnconfig -l | grep 'not in use' | head -1 |
 		cut -d ':' -f1)
 	_REL=$(echo ${_REL} | tr -d '.')
 
@@ -80,8 +80,8 @@ create_img() {
 	# syspatch(8)); we hardcode a 4G image because it's easy to extend /home
 	# if we need more space for specialized usage (or even add a new EBS)
 	pr_action "creating and mounting image filesystem"
-	doas vnconfig ${_VNDEV} ${_IMG}
-	doas fdisk -c 522 -h 255 -s 63 -yi ${_VNDEV}
+	vnconfig ${_VNDEV} ${_IMG}
+	fdisk -c 522 -h 255 -s 63 -yi ${_VNDEV}
 	cat <<'EOF' >${_WRKDIR}/disklabel
 type: SCSI
 disk: SCSI disk
@@ -107,19 +107,19 @@ boundend: 8385930
   h:          4192960          3984096  4.2BSD   2048 16384     1 
   i:           211552          8177056  4.2BSD   2048 16384     1
 EOF
-	doas disklabel -R ${_VNDEV} ${_WRKDIR}/disklabel
+	disklabel -R ${_VNDEV} ${_WRKDIR}/disklabel
 	for _p in a d e f g h i; do
-		doas newfs /dev/r${_VNDEV}${_p}
+		newfs /dev/r${_VNDEV}${_p}
 	done
-	doas mount /dev/${_VNDEV}a ${_MNT}
-	doas install -d ${_MNT}/{tmp,var,usr,home}
-	doas mount /dev/${_VNDEV}d ${_MNT}/tmp
-	doas mount /dev/${_VNDEV}e ${_MNT}/var
-	doas mount /dev/${_VNDEV}f ${_MNT}/usr
-	doas install -d ${_MNT}/usr/{X11R6,local}
-	doas mount /dev/${_VNDEV}g ${_MNT}/usr/X11R6
-	doas mount /dev/${_VNDEV}h ${_MNT}/usr/local
-	doas mount /dev/${_VNDEV}i ${_MNT}/home
+	mount /dev/${_VNDEV}a ${_MNT}
+	install -d ${_MNT}/{tmp,var,usr,home}
+	mount /dev/${_VNDEV}d ${_MNT}/tmp
+	mount /dev/${_VNDEV}e ${_MNT}/var
+	mount /dev/${_VNDEV}f ${_MNT}/usr
+	install -d ${_MNT}/usr/{X11R6,local}
+	mount /dev/${_VNDEV}g ${_MNT}/usr/X11R6
+	mount /dev/${_VNDEV}h ${_MNT}/usr/local
+	mount /dev/${_VNDEV}i ${_MNT}/home
 
 	pr_action "fetching sets from ${MIRROR:##*//}"
 	( cd ${_WRKDIR} &&
@@ -131,86 +131,80 @@ EOF
 
 	pr_action "extracting sets"
 	for i in ${_WRKDIR}/*${_REL}.tgz ${_MNT}/var/sysmerge/{,x}etc.tgz; do
-		doas tar xzphf $i -C ${_MNT}
+		tar xzphf $i -C ${_MNT}
 	done
 
 	pr_action "installing MP kernel"
-	doas mv ${_WRKDIR}/bsd* ${_MNT}
-	doas mv ${_MNT}/bsd ${_MNT}/bsd.sp
-	doas mv ${_MNT}/bsd.mp ${_MNT}/bsd
-	doas chown 0:0 ${_MNT}/bsd*
+	mv ${_WRKDIR}/bsd* ${_MNT}
+	mv ${_MNT}/bsd ${_MNT}/bsd.sp
+	mv ${_MNT}/bsd.mp ${_MNT}/bsd
+	chown 0:0 ${_MNT}/bsd*
 
 	pr_action "installing ec2-init"
-	doas install -m 0555 -o root -g bin ${_WRKDIR}/ec2-init \
+	install -m 0555 -o root -g bin ${_WRKDIR}/ec2-init \
 		${_MNT}/usr/local/libexec/ec2-init
 
 	pr_action "removing downloaded files"
 	rm ${_WRKDIR}/*${_REL}.tgz ${_WRKDIR}/ec2-init
 
 	pr_action "creating devices"
-	( cd ${_MNT}/dev && doas sh ./MAKEDEV all )
+	( cd ${_MNT}/dev && sh ./MAKEDEV all )
 
 	pr_action "storing entropy for the initial boot"
-	doas dd if=/dev/random of=${_MNT}/var/db/host.random bs=65536 count=1 \
+	dd if=/dev/random of=${_MNT}/var/db/host.random bs=65536 count=1 \
 		status=none
-	doas dd if=/dev/random of=${_MNT}/etc/random.seed bs=512 count=1 \
+	dd if=/dev/random of=${_MNT}/etc/random.seed bs=512 count=1 \
 		status=none
-	doas chmod 600 ${_MNT}/var/db/host.random ${_MNT}/etc/random.seed
+	chmod 600 ${_MNT}/var/db/host.random ${_MNT}/etc/random.seed
 
 	pr_action "installing master boot record"
-	doas installboot -r ${_MNT} ${_VNDEV}
+	installboot -r ${_MNT} ${_VNDEV}
 
 	pr_action "configuring the image"
 	# XXX hardcoded
-	echo "https://ftp.fr.openbsd.org/pub/OpenBSD" | doas tee \
-		${_MNT}/etc/installurl
-	_duid=$(doas disklabel ${_VNDEV} | grep duid | cut -d ' ' -f 2)
-	echo "${_duid}.b none swap sw" | doas tee ${_MNT}/etc/fstab
-	echo "${_duid}.a / ffs rw 1 1" | doas tee -a ${_MNT}/etc/fstab
-	echo "${_duid}.i /home ffs rw,nodev,nosuid 1 2" | doas tee -a \
-		${_MNT}/etc/fstab
-	echo "${_duid}.d /tmp ffs rw,nodev,nosuid 1 2" | doas tee -a \
-		${_MNT}/etc/fstab
-	echo "${_duid}.f /usr ffs rw,nodev 1 2" | doas tee -a ${_MNT}/etc/fstab
-	echo "${_duid}.g /usr/X11R6 ffs rw,nodev 1 2" | doas tee -a \
-		${_MNT}/etc/fstab
-	echo "${_duid}.h /usr/local ffs rw,wxallowed,nodev 1 2" | doas tee -a \
-		${_MNT}/etc/fstab
-	echo "${_duid}.e /var ffs rw,nodev,nosuid 1 2" | doas tee -a \
-		${_MNT}/etc/fstab
-	doas sed -i "s,^tty00.*,tty00	\"/usr/libexec/getty std.9600\"	vt220   on  secure," \
+	echo "https://ftp.fr.openbsd.org/pub/OpenBSD" >${_MNT}/etc/installurl
+	_duid=$(disklabel ${_VNDEV} | grep duid | cut -d ' ' -f 2)
+	echo "${_duid}.b none swap sw" >${_MNT}/etc/fstab
+	echo "${_duid}.a / ffs rw 1 1" >>${_MNT}/etc/fstab
+	echo "${_duid}.i /home ffs rw,nodev,nosuid 1 2" >>${_MNT}/etc/fstab
+	echo "${_duid}.d /tmp ffs rw,nodev,nosuid 1 2" >>${_MNT}/etc/fstab
+	echo "${_duid}.f /usr ffs rw,nodev 1 2" >>${_MNT}/etc/fstab
+	echo "${_duid}.g /usr/X11R6 ffs rw,nodev 1 2" >>${_MNT}/etc/fstab
+	echo "${_duid}.h /usr/local ffs rw,wxallowed,nodev 1 2" \
+		>>${_MNT}/etc/fstab
+	echo "${_duid}.e /var ffs rw,nodev,nosuid 1 2" >>${_MNT}/etc/fstab
+	sed -i "s,^tty00.*,tty00	\"/usr/libexec/getty std.9600\"	vt220   on  secure," \
 		${_MNT}/etc/ttys
-	echo "stty com0 9600" | doas tee ${_MNT}/etc/boot.conf
-	echo "set tty com0" | doas tee -a ${_MNT}/etc/boot.conf
-	echo "dhcp" | doas tee ${_MNT}/etc/hostname.xnf0
-	echo "!/usr/local/libexec/ec2-init" |
-		doas tee -a ${_MNT}/etc/hostname.xnf0
-	doas chmod 0640 ${_MNT}/etc/hostname.xnf0
-	echo "127.0.0.1\tlocalhost" | doas tee ${_MNT}/etc/hosts
-	echo "::1\t\tlocalhost" | doas tee -a ${_MNT}/etc/hosts
-	doas chroot ${_MNT} env -i ln -sf /usr/share/zoneinfo/UTC /etc/localtime
-	doas chroot ${_MNT} env -i ldconfig /usr/local/lib /usr/X11R6/lib
-	doas chroot ${_MNT} env -i rcctl disable sndiod
+	echo "stty com0 9600" >${_MNT}/etc/boot.conf
+	echo "set tty com0" >>${_MNT}/etc/boot.conf
+	echo "dhcp" >${_MNT}/etc/hostname.xnf0
+	echo "!/usr/local/libexec/ec2-init" >>${_MNT}/etc/hostname.xnf0
+	chmod 0640 ${_MNT}/etc/hostname.xnf0
+	echo "127.0.0.1\tlocalhost" >${_MNT}/etc/hosts
+	echo "::1\t\tlocalhost" >>${_MNT}/etc/hosts
+	chroot ${_MNT} ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+	chroot ${_MNT} ldconfig /usr/local/lib /usr/X11R6/lib
+	chroot ${_MNT} rcctl disable sndiod
 
-#	cat <<'EOF' | doas tee ${_MNT}/etc/hotplugd/attach
+#	cat <<-'EOF' >${_MNT}/etc/hotplug/attach
 ##!/bin/sh
 #
 #case $1 in
 #	3) /sbin/dhclient -i routers $2 ;;
 #esac
 #EOF
-#	doas chmod 0555 ${_MNT}/etc/hotplugd/attach
-#	doas chroot ${_MNT} env -i rcctl enable hotplugd
+#	chmod 0555 ${_MNT}/etc/hotplug/attach
+#	chroot ${_MNT} env -i rcctl enable hotplugd
 
 	pr_action "unmounting the image"
-	doas umount ${_MNT}/usr/X11R6
-	doas umount ${_MNT}/usr/local
-	doas umount ${_MNT}/usr
-	doas umount ${_MNT}/var
-	doas umount ${_MNT}/home
-	doas umount ${_MNT}/tmp
-	doas umount ${_MNT}
-	doas vnconfig -u ${_VNDEV}
+	umount ${_MNT}/usr/X11R6
+	umount ${_MNT}/usr/local
+	umount ${_MNT}/usr
+	umount ${_MNT}/var
+	umount ${_MNT}/home
+	umount ${_MNT}/tmp
+	umount ${_MNT}
+	vnconfig -u ${_VNDEV}
 	rm ${_WRKDIR}/disklabel
 
 	pr_action "image available at: ${_IMG}"
