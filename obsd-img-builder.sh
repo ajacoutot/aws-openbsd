cb01c7f0bd411dfe739f88c3edb3593f380dbdf7
Antoine Jacoutot <ajacoutot@gnome.org>
2019-03-22 16:36:51 +0100

    No need to set AWS_DEFAULT_PROFILE, it's the default.

--- obsd-img-builder.sh
+++ obsd-img-builder.sh
@@ -433,7 +433,6 @@ fi
 if ${CREATE_AMI}; then
 	[[ ${ARCH} == i386 ]] &&
 		pr_err "${ARCH} lacks xen(4) support to run on AWS"
-	readonly AWS_DEFAULT_PROFILE=${AWS_DEFAULT_PROFILE:-default}
 	type aws >/dev/null 2>&1 || pr_err "package \"awscli\" is not installed"
 	type vmdktool >/dev/null 2>&1 ||
 		pr_err "package \"vmdktool\" is not installed"
